11e19f12b4f2ab64e178e1d0c6ba6e0ce38ca58a
LeoDJ <leandro.spaeth@gmail.com>
2023-11-11 19:38:19 +0100

    add time functions

--- im-aimer-presence/src/main.cpp
+++ im-aimer-presence/src/main.cpp
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include "wifi_mac_sniffer.hpp"
-#include <time.h>
+#include "util.h"
 
 #define WIFI_CHANNEL_SWITCH_INTERVAL (500)
 #define WIFI_CHANNEL_MAX (13)
@@ -11,42 +11,6 @@ int mac_count = 0;
 uint8_t level = 0, channel = 1;
 static wifi_country_t wifi_country = {.cc = "DE", .schan = 1, .nchan = 13}; //Most recent esp32 library struct
 
-const char* ntpServer = "de.pool.ntp.org";
-const char* TZ_INFO =   "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00"; // Berlin, for others, see: https://remotemonitoringsystems.ca/time-zone-abbreviations.php
-tm timeinfo;
-time_t now;
-
-bool getNTPtime(int sec) {
-    {
-        uint32_t start = millis();
-        do {
-            time(&now);
-            localtime_r(&now, &timeinfo);
-            delay(10);
-        } while (((millis() - start) <= (1000 * sec)) && (timeinfo.tm_year < (2016 - 1900)));
-        if (timeinfo.tm_year <= (2016 - 1900))
-            return false; // the NTP call was not successful
-    }
-    return true;
-}
-
-char curISOStr[32] = {0};
-const char *getISOTimeStr() {
-    getNTPtime(0);
-    memset(curISOStr, 0, sizeof(curISOStr));
-    strftime(curISOStr, sizeof(curISOStr), "%FT%TZ", localtime(&now));  // Formats: http://www.cplusplus.com/reference/ctime/strftime/
-    return curISOStr;
-}
-
-void initNTP() {
-    configTime(0, 0, ntpServer);
-    setenv("TZ", TZ_INFO, 1);
-    if (getNTPtime(3)) {
-    } // wait up to 3sec to sync
-    else {
-        // DEBUG_println("Time not set");
-    }
-}
 
 WiFiMACSniffer sniffer;
 
@@ -57,6 +21,9 @@ void prom_pkt_callback(void *buf, wifi_promiscuous_pkt_type_t type) { sniffer.pr
 void setup() {
     Serial.begin(115200);
     Serial.println("AImer Hello World!");
+
+    // probably need WiFi first
+    initNTP();
 
     tcpip_adapter_init();
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
@@ -70,19 +37,6 @@ void setup() {
     esp_wifi_set_promiscuous_rx_cb(prom_pkt_callback);
 }
 
-void loop() {
-    // delay(WIFI_CHANNEL_SWITCH_INTERVAL);
-    // esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
-    // channel = (channel % WIFI_CHANNEL_MAX) + 1;
-    // if (channel == 1) {
-    //     Serial.printf("MACs (%d): ", mac_count);
-    //     for (int i = 0; i < mac_count; i++) {
-    //         Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x, ", mac_lib[i][0], mac_lib[i][1], mac_lib[i][2], mac_lib[i][3], mac_lib[i][4], mac_lib[i][5]);
-    //     }
-    //     Serial.printf("\n");
-        
-    //     mac_count = 0;
-    // }
-    
+void loop() {    
     sniffer.loop();
 }
